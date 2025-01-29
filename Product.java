dde0f9868b200e199109f7c45a368f6d125698b3
Austin150807 <mysql862@gmail.com>
2025-01-29 09:31:23 +0530

    Create Product.java

---
+++ Product.java
@@ -0,0 +1,10 @@
+class Product
+{
+  public static void main(String args[])
+  {
+    int a=11;
+    int b=77;
+    int product=a*b;
+    System.out.println(""product of the two numbers is"+product);
+  }
+}
